197b0335b6b57305f3de0b951f9d012db6f56609
andreykomov <komov.te@gmail.com>
2024-10-13 20:02:23 +0200

    [task-0002] added the zlib service

---
+++ src/services/zlibService.js
@@ -0,0 +1,53 @@
+import { createBrotliCompress, createBrotliDecompress } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
+class ZlibService {
+  loggerService;
+  fileService;
+  currentDirectory;
+
+  constructor(loggerService, fileService) {
+    this.loggerService = loggerService;
+    this.fileService = fileService;
+    this.currentDirectory = this.fileService.getCurrentDirectory();
+  }
+
+  compress(pathToFile, pathToDestination) {
+    try {
+      const readableStream = createReadStream(pathToFile);
+      const writeableStream = createWriteStream(pathToDestination);
+      const brotliCompress = createBrotliCompress();
+
+      writeableStream.on('finish', () => {
+        this.loggerService.log(`Success: File ${pathToFile} compressed to ${pathToDestination}.`);
+        this.loggerService.showCurrentDirectory(this.currentDirectory);
+      });
+      readableStream.on('error', (err) => this.loggerService.log(err.message));
+      writeableStream.on('error', (err) => this.loggerService.log(err.message));
+      readableStream.pipe(brotliCompress).pipe(writeableStream);
+    } catch (err) {
+      this.loggerService.log(err.message);
+
+    }
+  }
+
+  decompress(pathToFile, pathToDestination) {
+    try {
+      const readableStream = createReadStream(pathToFile);
+      const writeableStream = createWriteStream(pathToDestination);
+      const brotliDecompress = createBrotliDecompress();
+
+      writeableStream.on('finish', () => {
+        this.loggerService.log(`Success: File ${pathToFile} decompressed to ${pathToDestination}.`);
+        this.loggerService.showCurrentDirectory(this.currentDirectory);
+      });
+      readableStream.on('error', (err) => this.loggerService.log(err.message));
+      writeableStream.on('error', (err) => this.loggerService.log(err.message));
+      readableStream.pipe(brotliDecompress).pipe(writeableStream);
+    } catch (err) {
+      this.loggerService.log(err.message);
+    }
+  }
+}
+
+export default ZlibService;
